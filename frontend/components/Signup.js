cb2b9094055c42c7c832bd27de3d25b56244f19f
GoranBT <goran.angelovski.bt@gmail.com>
2019-03-12 00:36:45 +0100

    typo

--- frontend/components/Signup.js
+++ frontend/components/Signup.js
@@ -3,7 +3,7 @@ import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import Form from './styles/Form';
 import Error from './ErrorMessage';
-import { CURRENT_USER_QUERY } from './user';
+import { CURRENT_USER_QUERY } from './User';
 
 const SIGNUP_MUTATION = gql`
   mutation SIGNUP_MUTATION(
